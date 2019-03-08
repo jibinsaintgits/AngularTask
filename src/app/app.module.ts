22703cac3df737849ac32bc056e3eef903f43abc
jibin varghese <jibinskpm123@gmail.com>
2019-03-08 12:46:20 +0530

    Angular task

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
+import { HomeComponent } from './home/home.component';
+import { InvalidComponent } from './invalid/invalid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Demo1Component,
+    Demo2Component,
+    HomeComponent,
+    InvalidComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
